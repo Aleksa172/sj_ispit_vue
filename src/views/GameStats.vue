1cf6be3e97f9594501d0fb836ad6595c40b7b36d
Aleksa Smiljanic <asmiljanic16@raf.rs>
2021-02-11 01:19:31 +0100

    dodat probni chart

---
+++ src/views/GameStats.vue
@@ -0,0 +1,35 @@
+<template>
+    <div> Game stats 
+        <JSCharting :options="options" class="columnChart"></JSCharting>
+
+
+    </div>
+</template>
+
+<script>
+import JSCharting from 'jscharting-vue';
+
+
+export default {
+    data(){
+        return {
+            name: 'columnChart',
+            options: {
+                type: 'horizontal column',
+                series: [
+                    {
+                        points: [
+                            { x: 'A', y: 50 },
+                            { x: 'B', y: 30 },
+                            { x: 'C', y: 50 }
+                        ]
+                    }
+                ]
+            }
+        }
+    },
+    components: {
+        JSCharting
+    }
+}
+</script>
